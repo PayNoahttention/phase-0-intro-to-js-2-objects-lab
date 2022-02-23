eca95a74697c4e4e5de5ca4dab2438dfe92dcab6
NoahSolis <NSolis1993@gmail.com>
2022-02-23 14:34:53 -0600

    commit tree 183d6c0cb7e2d5d0de7c90cb35a303cfd6c2727c

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "", 
+    streetAddress: "",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeUpdate = {...employee};
+
+    employeeUpdate[key] = value;
+
+    return employeeUpdate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const deletedEmployee = {...employee};
+
+    deletedEmployee[key] = value;
+
+    return deletedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
